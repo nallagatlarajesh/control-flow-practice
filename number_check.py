97faa8fc63468cf043f6e7d6337dc4ef9bbbf49d
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2023-12-02 16:51:05 +0530

    Create number_check.py

---
+++ number_check.py
@@ -0,0 +1,8 @@
+##check whether a number is positive negative or zero 
+number=int(input("enrtr a number to be checked :"))
+if number >0:
+    print("number is positive ") 
+elif number<0:
+    print("number is negative ")
+else:
+    print("number is zero")
