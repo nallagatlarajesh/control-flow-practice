8d19cbcd536b42c595cfe6fcdc4b90f004cd36ca
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2023-12-02 17:23:14 +0530

    find large number
    its  for not only larger and also indentation problem ,the identation takes same place for two statements in the code

---
+++ large_number.py
@@ -0,0 +1,10 @@
+# program to find  larger of two numbers
+num1=5
+num2=6
+if num1<num2:
+    print("first number is larger ")
+    print("hai iam rajesh")
+else:
+    print("second number is larger")
+    print("hai iam jahn")
+    
