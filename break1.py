526aa9f4851d13858d5b99cf7237bd701be117ea
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2023-12-03 01:55:15 +0530

    Create break1.py

---
+++ break1.py
@@ -0,0 +1,8 @@
+#program to demonstrate  use of breakk stattement
+num=0
+for num in range(10):
+    num=num+1
+    if num==8:
+        break
+    print("num has value ",num)
+print("encounterd break !! out of loop")
