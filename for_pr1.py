c12d292a89b295347608ca4a4abd6d1bcd6569ca
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2023-12-03 00:09:05 +0530

    Create for_pr1.py

---
+++ for_pr1.py
@@ -0,0 +1,8 @@
+#program even numbers in the given sequence
+numbers=[1,2,3,4,5,6,7,8,9]
+for num in numbers:
+    if (num%2)==0:
+        print(num,"is an even number")
+    #else:
+        #print(num,"is an odd number")
+        
